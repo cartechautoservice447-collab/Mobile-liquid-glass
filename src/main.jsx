47f61523442d67022d15daf0a26961a6852a87ab
cartechautoservice447-collab <cartechautoservice447@gmail.com>
2026-09-08 00:46:18 +0530

    cleanup: remove superseded glass sync stylesheet

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,6 @@ import './LiquidGlassEngine.css';
 import './LiquidGlassFinalOverrides.css';
 import './DashboardResponsive.css';
 import './CourseWorkspaceSpacing.css';
-import './GlassThemeSurfaceSync.css';
 import './GlassSurfaceRuntimeLock.js';
 
 createRoot(document.getElementById('root')).render(
