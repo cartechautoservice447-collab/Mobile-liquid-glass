9c4932bfcb7616a8a938f36f5548bb6ce8687d98
cartechautoservice447-collab <cartechautoservice447@gmail.com>
2026-09-12 21:54:26 +0530

    fix: load collection hydration retry runtime

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import './workspaceHydrationRepair.js';
 import './collectionTombstoneRepair.js';
 import './collectionDeleteRuntimeRepair.js';
+import './collectionDeleteHydrationRetry.js';
 import App from './App.jsx';
 import { configureNativeAuth } from './nativeAuth.js';
 import { configureSharedNotifications } from './sharedNotifications.js';
