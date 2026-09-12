1764a844f73bc15b1a8a6694da5018e897f63e38
cartechautoservice447-collab <cartechautoservice447@gmail.com>
2026-09-12 18:56:05 +0530

    fix: run workspace collection repair before hydration

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import './workspaceHydrationRepair.js';
 import App from './App.jsx';
 import { configureNativeAuth } from './nativeAuth.js';
 import { configureSharedNotifications } from './sharedNotifications.js';
